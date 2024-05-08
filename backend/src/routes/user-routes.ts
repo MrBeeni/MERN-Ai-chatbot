70ef535c6a389a843155e01804f5085dcd8f3608
MrBeeni <mubeen4429@gmail.com>
2024-05-08 15:48:34 +0500

    chore: Add frontend pages and components
    chat functionality

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -3,6 +3,7 @@ import {
   getAllUsers,
   userLogin,
   userSignup,
+  verifyUser,
   // userLogout,
   // verifyUser,
 } from "../controllers/user-controllers.js";
@@ -11,14 +12,14 @@ import {
   signupValidator,
   validate,
 } from "../utils/validators.js";
-// import { verifyToken } from "../utils/token-manager.js";
+import { verifyToken } from "../utils/token-manager.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), userSignup);
 userRoutes.post("/login", validate(loginValidator), userLogin);
-// userRoutes.get("/auth-status", verifyToken, verifyUser);
+userRoutes.get("/auth-status", verifyToken, verifyUser);
 // userRoutes.get("/logout", verifyToken, userLogout);
 
 export default userRoutes;
